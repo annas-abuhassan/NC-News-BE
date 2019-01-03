1154f68cbcbada4b1007124cbb5123ba376d1ebe
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2019-01-03 21:03:07 +0000

    fixed log import, set log type to debug

--- listen.js
+++ listen.js
@@ -1,8 +1,8 @@
 const PORT = process.env.PORT || 9090;
-const { log } = require('./logger.js');
+const log = require('./logger.js');
 const app = require('./app');
 
 app.listen(PORT, err => {
   if (err) throw err;
-  log.info(`Listening on port: ${PORT}`);
+  log.debug(`Listening on port: ${PORT}`);
 });
