e5b04e5c1f0a6638c80deda62333c9fd718f2ba7
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2019-01-03 17:25:08 +0000

    back to just one logger, delete comments later

--- logger.js
+++ logger.js
@@ -1,7 +1,8 @@
 const { createLogger, format, transports } = require('winston');
-const expressWinston = require('express-winston');
+// const expressWinston = require('express-winston');
+
 const log = createLogger({
-  level: process.env.LOGGING_LEVEL || 'info',
+  level: process.env.LOGGING_LEVEL || 'debug',
   format: format.combine(
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
@@ -10,7 +11,7 @@ const log = createLogger({
   ),
   transports: [
     new transports.Console({
-      level: 'info',
+      level: 'debug',
       format: format.combine(
         format.colorize(),
         format.printf(
@@ -22,28 +23,52 @@ const log = createLogger({
   ]
 });
 
-const httpLog = expressWinston.logger({
-  level: process.env.LOGGING_LEVEL || 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.json()
-  ),
-  transports: [
-    new transports.Console({
-      level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-    new transports.File({ filename: `${__dirname}/log.json` })
-  ],
-  meta: false,
-  msg: `Method: '{{req.method}}' URL: '{{req.url}}' STATUS CODE: '{{res.statusCode}}' RESPONSE-TIME: '{{res.responseTime}}ms'`
-});
+// const httpLog = expressWinston.logger({
+//   // level: process.env.LOGGING_LEVEL || 'info',
+//   format: format.combine(
+//     format.timestamp({
+//       format: 'YYYY-MM-DD HH:mm:ss'
+//     }),
+//     format.json()
+//   ),
+//   transports: [
+//     new transports.Console({
+//       level: 'info',
+//       format: format.combine(
+//         format.colorize(),
+//         format.printf(
+//           info => `${info.timestamp} ${info.level}: ${info.message}`
+//         )
+//       )
+//     })
+//     // new transports.File({ filename: `${__dirname}/log.json` })
+//   ],
+//   meta: false,
+//   msg: `Method: '{{req.method}}' URL: '{{req.url}}' STATUS CODE: '{{res.statusCode}}' RESPONSE-TIME: '{{res.responseTime}}ms'`
+// });
 
-module.exports = { log, httpLog };
+// const errorLog = expressWinston.errorLogger({
+//   // level: process.env.LOGGING_LEVEL || 'error',
+//   format: format.combine(
+//     format.timestamp({
+//       format: 'YYYY-MM-DD HH:mm:ss'
+//     }),
+//     format.json()
+//   ),
+//   transports: [
+//     new transports.Console({
+//       level: 'error',
+//       format: format.combine(
+//         format.colorize(),
+//         format.printf(error => {
+//           console.log(error);
+//           return `${error.timestamp} ${error.level}: ${error.message}`;
+//         })
+//       )
+//     })
+//   ],
+//   meta: false,
+//   msg: `Method: '{{req.method}}' URL: '{{req.url}}' STATUS CODE: '{{res.statusCode}}' RESPONSE-TIME: '{{res.responseTime}}ms'`
+// });
+
+module.exports = { log, httpLog, errorLog };
