621011a9c40695a6668993439b77e76248ce665c
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2019-01-03 14:58:23 +0000

    Made secondary logger with express-winston to handle the HTTP requests, not sure if i still need two loggers

--- logger.js
+++ logger.js
@@ -1,5 +1,5 @@
 const { createLogger, format, transports } = require('winston');
-
+const expressWinston = require('express-winston');
 const log = createLogger({
   level: process.env.LOGGING_LEVEL || 'info',
   format: format.combine(
@@ -22,4 +22,28 @@ const log = createLogger({
   ]
 });
 
-module.exports = log;
+const httpLog = expressWinston.logger({
+  level: process.env.LOGGING_LEVEL || 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename: `${__dirname}/log.json` })
+  ],
+  meta: false,
+  msg: `Method: '{{req.method}}' URL: '{{req.url}}' STATUS CODE: '{{res.statusCode}}' RESPONSE-TIME: '{{res.responseTime}}ms'`
+});
+
+module.exports = { log, httpLog };
