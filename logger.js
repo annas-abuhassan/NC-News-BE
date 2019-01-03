85e722c88ad48d7837ee822ad14d2b8ea2744986
Annas Abu-Hassan <annas.abuhassan@gmail.com>
2019-01-03 13:46:43 +0000

    logs are writing to JSON file

--- logger.js
+++ logger.js
@@ -3,13 +3,23 @@ const { createLogger, format, transports } = require('winston');
 const log = createLogger({
   level: 'debug',
   format: format.combine(
-    format.colorize(),
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    format.json()
   ),
-  transports: [new transports.Console()]
+  transports: [
+    new transports.Console({
+      level: 'info',
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          info => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      )
+    }),
+    new transports.File({ filename: `${__dirname}/log.json` })
+  ]
 });
 
 module.exports = log;
